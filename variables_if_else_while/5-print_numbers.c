1bffb52d22e67567a2740b48053fc99cfe99c0ce
Dawson <dawsonzhef@gmail.com>
2023-05-29 08:31:25 -0700

    2-5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.>
+
+/**
+ * main - prints single digit numbers starting from 0
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
+
