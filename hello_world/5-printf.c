774c8c0db4b0605179ce6d56e135d3267cd5ace9
Dawson <dawsonzhef@gmail.com>
2023-05-29 06:53:09 -0700

    5 rr

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of ar,\n"
+ *
+ * return: always 0
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
