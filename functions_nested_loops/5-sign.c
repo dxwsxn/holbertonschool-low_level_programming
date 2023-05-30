88445d0490b6e15a8abe729cdb9364f96ceeab7b
Dawson <dawsonzhef@gmail.com>
2023-05-30 10:29:52 -0700

    3-5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_sign - printing sign
+ * @i: description
+ * Return: 0 or 1 or -1
+ */
+
+int print_sign(int i)
+{
+	if (i > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (i < 0)
+	{
+		_putchar('-');
+		return (1);
+	}
+	else
+	{
+		_putchar ('0');
+		return (0);
+	}
+}
