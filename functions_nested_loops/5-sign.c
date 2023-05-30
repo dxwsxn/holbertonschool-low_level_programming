1f683ccf7cc9626d52e04934b4258f60c1f882af
Dawson <dawsonzhef@gmail.com>
2023-05-30 14:36:42 -0700

    3-5 bety

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -14,14 +14,14 @@ int print_sign(int i)
 		_putchar('+');
 		return (1);
 	}
-	else if (i < 0)
+	else if (i == 0)
 	{
-		_putchar('-');
+		_putchar('0');
 		return (1);
 	}
 	else
 	{
-		_putchar ('0');
-		return (0);
+		_putchar ('-');
+		return (-1);
 	}
 }
