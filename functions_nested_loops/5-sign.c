c4d87ba948b5acc72b127b8456634e0ad816587f
Dawson <dawsonzhef@gmail.com>
2023-05-30 10:37:11 -0700

    3-5 betty pleasing

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int i)
 {
 	if (i > 0)
 	{
-		_putchar('+');
+		putchar('+');
 		return (1);
 	}
 	else if (i < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		return (1);
 	}
 	else
 	{
-		_putchar ('0');
+		putchar ('0');
 		return (0);
 	}
 }
