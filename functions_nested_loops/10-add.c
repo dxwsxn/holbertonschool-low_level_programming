4d258a4d343a90c4ef646dc02e2108bc04c8e0e0
Dawson <dawsonzhef@gmail.com>
2023-05-30 16:51:47 -0700

    3-10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add -fuinction that adds
+ * @I1: first numbah
+ * @I2: second numbah
+ *
+ * Return: result of function
+ */
+
+int add(int I1, int I2)
+{
+	return (I1 + I2);
+}
