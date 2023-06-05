1d00719a1aca2135c3e443b35c63c5a7f310f4b5
Dawson <dawsonzhef@gmail.com>
2023-06-05 08:16:01 -0700

    Almight push 2.0

---
+++ more_functions_nested_loops/4-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main- code check
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_most_number();
+	return (0);
+}
---
+++ more_functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
