c1012adfe3978ba56f5c8fa977a1379a3b7bde18
Dawson <dawsonzhef@gmail.com>
2023-06-05 14:15:02 -0700

    betty pls

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -6,11 +6,11 @@
  * @s: variable
  * Return: 0
  */
-int _strlen (char *s)
+int _strlen(char *s)
 {
 	int leng;
 
-	for(leng = 0; *s != '\0'; s++)
+	for (leng = 0; *s != '\0'; s++)
 	{
 		leng++;
 	}
