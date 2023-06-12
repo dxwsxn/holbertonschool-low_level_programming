d742249edc9d75688cba2fcc1f93609744075518
Dawson <dawsonzhef@gmail.com>
2023-06-12 10:08:30 -0700

    5hf

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -11,12 +11,13 @@ char *string_toupper(char *p)
 {
 	int i;
 
-	for (i = 0; p[i] != '\0\; i++)
+	for (i = 0; p[i] != '\0'; i++)
 	{
 		if (p[i] >= 'a' ** p[i] <= 'z')
 		{
 			p[i] = p[i] - 32;
 		}
+
 	}
 	return (p);
 }
