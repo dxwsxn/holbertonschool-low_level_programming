38383126f96919b4cf659b58a1639c7ddae275d1
Dawson <dawsonzhef@gmail.com>
2023-06-12 09:50:34 -0700

    1hf

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,21 +10,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-		int s, d
-			;
-		s = 0;
+		int s1 = 0; 
+		int s2 =0;
 
-		while (dest[s])
-		s++;
-
-		for (d = 0; d < n; d++)
+		while (dest[s1] != '\0')
 		{
-			if (src[d])
-			{
-				dest[s + d] = src[d];
+			s++;
 		}
-		else
-			break;
-}
-return (dest);
+		while (src[s2] != '\0')
+		{
+			if (s2 < n)
+			dest[s1] = src[s2];
+			s1++;
+			s2++;
+		}
+		dest[s1] = '\0';
+		return (dest);
 }
