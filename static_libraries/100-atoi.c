593f6e01d6161e3b1a848cfd0385bd270dbaec17
Dawson <dawsonzhef@gmail.com>
2023-06-29 08:44:46 -0700

    new

--- static_libraries/100-atoi.c
+++ static_libraries/100-atoi.c
@@ -1,1 +1,22 @@
 #include "main.h"
+int _atoi(char *s)
+/**
+ * _atoi - convert str to int
+ * @s: str being convert
+ *
+ * Return: the int convert from str
+ */
+
+int _atoi(char *s)
+{
+	int i, d, n, leng, f, num;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	leng = 0;
+	f = 0;
+	num = 0;
+
+	while (s[leng] != '\0'}
+			
