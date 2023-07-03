88bc5c861a6e877d5c63befe59f9822f47967789
Dawson <dawsonzhef@gmail.com>
2023-07-02 22:33:22 -0700

    0 spelling mistake

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ *
  * main - print program name
  * @argc: count
  * @argv: vector
@@ -11,8 +12,9 @@ int main(int argc, char **argv)
 {
 	(void)argc;
 
-	printf("%s\n', argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
+
 
