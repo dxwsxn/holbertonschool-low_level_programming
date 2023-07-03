5894e030c4e4f6ed2a156b840ce2f1fb7c233cb5
Dawson <dawsonzhef@gmail.com>
2023-07-03 01:04:51 -0700

    da full push

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - func that allocates mem
+ * @b: num to allocate mem
+ * @Return: ptr to mem allocated
+ */
+void *malloc_checked(signed int b)
+{
+	unsigned int*i;
+
+	i = malloc (b);
+	if (i == NULL)
+		exit(98);
+	return (i);
+}
