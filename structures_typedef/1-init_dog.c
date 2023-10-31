ce556ca50c1a81ea65b2f34c54f8085f6d2dfffe
dawson <dawsonzhef@gmail.com>
2023-10-30 17:31:12 -0700

    1 betty issue

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -5,7 +5,7 @@
  * @d: struct to init
  * @name: name of dog
  * @age: age of dog
- * @age: age of dog
+ * @owner: owner of dog
  * Return: nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
